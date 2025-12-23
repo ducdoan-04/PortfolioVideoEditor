2d52d396e3f6f86454abdd7033a1ac75a3cac50e
ducdoan-04 <hoducdoan2004@gmail.com>
2025-12-23 20:10:15 +0700

    change font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,24 +1,32 @@
-import type { Metadata } from "next";
-import { GeistSans } from "geist/font/sans";
-import { GeistMono } from "geist/font/mono";
-import "./globals.css";
+import type { Metadata } from 'next'
+import { GeistSans } from 'geist/font/sans'
+import { GeistMono } from 'geist/font/mono'
+import './globals.css'
 
 export const metadata: Metadata = {
-  title: "Ho Duc Doan",
-  description: "Created with jsx & node.js",
-};
+  title: 'Ho Duc Doan',
+  description: 'Created with jsx & node.js',
+  generator: '',
+}
 
 export default function RootLayout({
   children,
-}: {
-  children: React.ReactNode;
-}) {
+}: Readonly<{
+  children: React.ReactNode
+}>) {
   return (
-    <html
-      lang="en"
-      className={`${GeistSans.variable} ${GeistMono.variable}`}
+    <html lang="en"
     >
+      <head>
+        <style>{`
+html {
+  font-family: ${GeistSans.style.fontFamily};
+  --font-sans: ${GeistSans.variable};
+  --font-mono: ${GeistMono.variable};
+}
+        `}</style>
+      </head>
       <body>{children}</body>
     </html>
-  );
+  )
 }
