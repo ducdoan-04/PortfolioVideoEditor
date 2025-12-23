9b8bdfa90f4379b063776b3b6463a76e6b091b8f
ducdoan-04 <hoducdoan2004@gmail.com>
2025-12-23 20:00:24 +0700

    fix error font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,31 +1,24 @@
-import type { Metadata } from 'next'
-import { GeistSans } from 'geist/font/sans'
-import { GeistMono } from 'geist/font/mono'
-import './globals.css'
+import type { Metadata } from "next";
+import { GeistSans } from "geist/font/sans";
+import { GeistMono } from "geist/font/mono";
+import "./globals.css";
 
 export const metadata: Metadata = {
-  title: 'Ho Duc Doan',
-  description: 'Created with jsx & node.js',
-  generator: '',
-}
+  title: "Ho Duc Doan",
+  description: "Created with jsx & node.js",
+};
 
 export default function RootLayout({
   children,
-}: Readonly<{
-  children: React.ReactNode
-}>) {
+}: {
+  children: React.ReactNode;
+}) {
   return (
-    <html lang="en">
-      <head>
-        <style>{`
-html {
-  font-family: ${GeistSans.style.fontFamily};
-  --font-sans: ${GeistSans.variable};
-  --font-mono: ${GeistMono.variable};
-}
-        `}</style>
-      </head>
+    <html
+      lang="en"
+      className={`${GeistSans.variable} ${GeistMono.variable}`}
+    >
       <body>{children}</body>
     </html>
-  )
+  );
 }
