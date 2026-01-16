43944360e8cc531308e6b23cc8b8c2fc363b08a9
ducdoan-04 <hoducdoan2004@gmail.com>
2026-01-16 14:26:34 +0700

    update image and fix cros new domain2

--- backend-new/server.js
+++ backend-new/server.js
@@ -24,8 +24,7 @@ app.use(morgan('combined'));
 const allowedOrigins = [
   'http://localhost:3000',
   'https://portfolio-video-editor.onrender.com',
-  'https://portfolio-video-editor.vercel.app',
-  'https://ducdoan04-editor.vercel.app/'
+  'https://ducdoan04-editor.vercel.app',
 ];
 
 // CORS configuration
